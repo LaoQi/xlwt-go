50e3192d97aefb6df5512c2840721e9cf60fd3f3
LaoQi <me@madao.dev>
2025-05-23 14:14:39 +0800

    fix SharedStringTable

--- SharedStringTable.go
+++ SharedStringTable.go
@@ -3,7 +3,12 @@ package xlwt
 import (
 	"bytes"
 	"encoding/binary"
+	"log"
 )
+
+const MaxSSTLength = 0x2020
+const MaxSSTCellLength = 0x2000
+const CONTINUE_ID = 0x003C
 
 type SharedStringTable struct {
 	StrIndexes map[string]int
@@ -29,21 +34,49 @@ func (sst *SharedStringTable) AddStr(value string) int {
 	return idx
 }
 
-func (sst *SharedStringTable) GetSSTRecord() []byte {
-	var buf bytes.Buffer
-	for _, str := range sst.StrList {
-		buf.Write(U16StringPack(str))
-	}
-	return buf.Bytes()
-}
-
 func (sst *SharedStringTable) GetBiffRecord() []byte {
 	var buf bytes.Buffer
 	_ = binary.Write(&buf, binary.LittleEndian, SP_H(0x00FC)) // SST
-	body := sst.GetSSTRecord()
-	_ = binary.Write(&buf, binary.LittleEndian, SP_H(len(body)+8))
-	_ = binary.Write(&buf, binary.LittleEndian, SP_I(sst.Total))
-	_ = binary.Write(&buf, binary.LittleEndian, SP_I(len(sst.StrList)))
-	buf.Write(body)
+	//firstLen, body := sst.GetSSTRecord()
+
+	var tmp bytes.Buffer
+	firstLen := 8
+	hasWriteFirst := false
+	for _, str := range sst.StrList {
+		//buf.Write(U16StringPack(str))
+		block := U16StringPack(str)
+		if len(block) >= MaxSSTCellLength {
+			log.Println("exceed max sst cell length", len(block))
+			continue
+		}
+		if tmp.Len()+len(block) > MaxSSTLength {
+			if !hasWriteFirst {
+				hasWriteFirst = true
+				firstLen = firstLen + tmp.Len()
+				_ = binary.Write(&buf, binary.LittleEndian, SP_H(firstLen))
+				_ = binary.Write(&buf, binary.LittleEndian, SP_I(sst.Total))
+				_ = binary.Write(&buf, binary.LittleEndian, SP_I(len(sst.StrList)))
+			} else {
+				//	write continue
+				_ = binary.Write(&buf, binary.LittleEndian, SP_H(CONTINUE_ID))
+				_ = binary.Write(&buf, binary.LittleEndian, SP_H(tmp.Len()))
+			}
+			buf.Write(tmp.Bytes())
+			tmp.Reset()
+		}
+		tmp.Write(block)
+	}
+
+	if !hasWriteFirst {
+		firstLen = firstLen + tmp.Len()
+		_ = binary.Write(&buf, binary.LittleEndian, SP_H(firstLen))
+		_ = binary.Write(&buf, binary.LittleEndian, SP_I(sst.Total))
+		_ = binary.Write(&buf, binary.LittleEndian, SP_I(len(sst.StrList)))
+	} else {
+		_ = binary.Write(&buf, binary.LittleEndian, SP_H(CONTINUE_ID))
+		_ = binary.Write(&buf, binary.LittleEndian, SP_H(tmp.Len()))
+	}
+	buf.Write(tmp.Bytes())
+
 	return buf.Bytes()
 }
